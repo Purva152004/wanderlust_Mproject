addaef4af92b7aaf72844347a8a731d2aceb4805
Purva Potbhare <purvapotbhare4@gmail.com>
2024-12-30 13:41:37 +0530

    Add Project Files

---
+++ routes/review.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router=express.Router({mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Review = require('../models/reviews.js');
+const Listing = require('../models/listing');
+const {validateReview,isLoggedIn,isReviewAuthor}=require("../middleware.js");
+
+const reviewController=require("../controllers/reviews.js");
+
+
+//POST review Route
+router.post("/",isLoggedIn,validateReview, wrapAsync(reviewController.createReview));
+ 
+//delete review route
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
+
+ 
+module.exports=router;
+ 
